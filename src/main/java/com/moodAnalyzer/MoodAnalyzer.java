e84ca959023ea05caa81ae29e08a3500531aea9d
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-03 05:13:42 +0530

    ADDED:Null ExceptionPointer Exception

--- src/main/java/com/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/com/moodAnalyzer/MoodAnalyzer.java
@@ -2,16 +2,25 @@ package com.moodAnalyzer;
 
 public class MoodAnalyzer {
     public String message;
+    //Creating Two Constructors of Non-Parameterized And Non-Parameterized
     public MoodAnalyzer(){
     }
     public MoodAnalyzer(String message){
         this.message= message;
     }
+    //Checking The User Is Sad
     public String moodAnalyzer(String message){
-        if (message.contains("Sad"))
-            return "SAD";
-        else
-            return "HAPPY";
+        //Checking For Exception If Any
+        try {
+            if (message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+        //Catching Null Pointer Exception
+        catch (NullPointerException e){
+            return "True";
+        }
     }
     
 }
