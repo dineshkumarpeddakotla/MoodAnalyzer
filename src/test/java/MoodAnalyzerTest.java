a89d4478e73f6654f7f14970d42b4bc41626f8e4
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-03 11:51:59 +0530

    ADDED:Custom Exception Method To Test Null Mood Method

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,28 +1,36 @@
 import com.moodAnalyzer.MoodAnalyzer;
-
+import com.moodAnalyzer.MoodAnalyzerException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() {
+    public void givenMessage_WhenSad_ShouldReturnSad() throws MoodAnalyzerException {
         MoodAnalyzer moodTest = new MoodAnalyzer();
         String mood = moodTest.moodAnalyzer("I am in Sad Mood");
         Assertions.assertEquals("SAD",mood);
-
     }
-
     @Test
-    public void givenMessage_WhenHappy_ShouldReturnHappy() {
+    public void givenMessage_WhenHappy_ShouldReturnHappy() throws MoodAnalyzerException {
         MoodAnalyzer moodTest = new MoodAnalyzer();
-        String mood = moodTest.moodAnalyzer("I am in Happy Mood");
+        String mood = moodTest.moodAnalyzer("I am in Any Mood");
         Assertions.assertEquals("HAPPY",mood);
     }
-
-    @Test
+    @SuppressWarnings("deprecation")
+    @Test()
     public void givenNull_WhenNull_ThrowsException() {
-        MoodAnalyzer moodTest = new MoodAnalyzer();
-        String mood = moodTest.moodAnalyzer(null);
-        Assertions.assertEquals("HAPPY", mood);
+        MoodAnalyzer moodTest = new MoodAnalyzer(null);
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(MoodAnalyzerException.class);
+            String mood=moodTest.moodAnalyzer(null);
+            Assertions.assertEquals("NULL",mood);
+        }
+        catch (MoodAnalyzerException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
